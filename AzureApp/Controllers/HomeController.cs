499bae161f6e7cb8eabccc33a0739f9f60f5cab8
manikandankkr <32087032+manikandankkr@users.noreply.github.com>
2019-07-04 18:40:15 +0530

    Added support for SSH tunnel creation.

--- AzureApp/Controllers/HomeController.cs
+++ AzureApp/Controllers/HomeController.cs
@@ -131,7 +131,7 @@ namespace AzureWebApp.Controllers
                 connectionParameters.SshClient = new SshClient(conn);
                 connectionParameters.SshClient.Connect();
                 logs += string.Format("Connected with SSH Server \n");
-                connectionParameters.forwardedPortLocal = new ForwardedPortLocal("127.0.0.1", 5122, connectionParameters.HostName, (uint)connectionParameters.Port);
+                connectionParameters.forwardedPortLocal = new ForwardedPortLocal("127.0.0.1", connectionParameters.HostName, (uint)connectionParameters.Port);
                 connectionParameters.SshClient.AddForwardedPort(connectionParameters.forwardedPortLocal);
                 connectionParameters.forwardedPortLocal.Start();
                 logs += string.Format("SSH Tunnel created \n");                
@@ -148,7 +148,7 @@ namespace AzureWebApp.Controllers
             try
             {
                 var databases = mongoConnection.MongoDBServer.GetDatabaseNames().ToList();
-                logs += string.Format("\n\n Available databases in MongoDB Server:");
+                logs += string.Format("\n Available databases in MongoDB Server:\n");
                 foreach(string database in databases)
                     logs += string.Format(database + "\n");
             }
@@ -188,97 +188,9 @@ namespace AzureWebApp.Controllers
         {           
             MongoClientSettings settings = new MongoClientSettings
             {
-                Server = new MongoServerAddress(connectionParameters.HostName, 5122),
+                Server = new MongoServerAddress(connectionParameters.HostName, (int)connectionParameters.forwardedPortLocal.BoundPort),
                 RetryWrites = true
-            };
-
-
-          ////  logs += "--- Basic MongoDB Settings has done. \n";
-
-          //  string hostname = connectionParameters.SshHostName;
-          // // logs += string.Format("--- Basic SSH Settings has done wih SSH host name {0} \n",hostname);
-          //  int port = int.Parse(connectionParameters.SshPort);
-          // // logs += string.Format("--- Basic SSH Settings has done wih SSH port name {0} \n", port);
-          //  string username = connectionParameters.SshUserName;
-          ////  logs += string.Format("--- Basic SSH Settings has done wih SSH UserName {0} \n", username);
-          //  string password = connectionParameters.SshPassword;
-          ////  logs += string.Format("--- Basic SSH Settings has done wih SSH Password {0} \n", password);
-          ////  logs += string.Format("--- Basic SSH Settings has done wih SSH Private key file {0} \n", connectionParameters.SshPrivateKey);
-          // string fullPath = Path.Combine(Server.MapPath("~/Files/certificates/"), connectionParameters.SshPrivateKey);
-          ////  logs += "--- Certificate file as readed as stream \n";
-            //try
-            //{
-
-            //    ConnectionInfo conn = new ConnectionInfo(hostname, port, username, new PrivateKeyAuthenticationMethod(username, new PrivateKeyFile(fullPath, password)));
-            //  //  logs += "--- SSH Connection setting has been prepared. \n";
-            //    SshClient cli = new SshClient(conn);
-            //  //  logs += "--- SSH Connection has been prepared. \n";
-            //    cli.Connect();
-            //  //  logs += "--- SSH Connection has been started. \n";
-            //}
-            //catch(Exception exception)
-            //{
-            //    logs += "--- SSH Connection has been stopeed. \n";
-            //    logs += string.Format("--- {0}\n",exception.Message);
-            //    if(exception.InnerException != null)
-            //    {
-            //        logs += string.Format("--- {0}\n", exception.InnerException.Message);
-            //    }
-            //}
-
-
-            //logs += "--- SSH Connection has been finished. \n";
-
-
-
-
-
-
-
-
-
-
-
-            //if (connectionParameters.IsSslEnabled &&
-            //    !connectionParameters.AuthenticationMechanism.Equals(Models.MongoAuthentication.X509))
-            //{
-            //    settings.VerifySslCertificate = !connectionParameters.IsSelfSignedEnabled;
-            //    connectionParameters.SshPrivateKeyData = FilePathHelper.ReadFile(Server, connectionParameters.SshPrivateKey);
-            //    logs += "--- SSL Certificate data has been retrieved. \n";
-            //    if (connectionParameters.SshPrivateKeyData != null)
-            //    {
-            //        logs += "--- SSL Certificate data not null. \n";
-            //        logs += "----CertificateData. \n" + System.Text.Encoding.UTF8.GetString(connectionParameters.SshPrivateKeyData);
-            //        var certificate = string.IsNullOrEmpty(connectionParameters.SshUserName) ? new X509Certificate2(connectionParameters.SshPrivateKeyData) : new X509Certificate2(connectionParameters.SshPrivateKeyData, connectionParameters.SshUserName);
-            //        logs += "----CertificateAdded. \n";
-            //        settings.SslSettings = new SslSettings()
-            //        {
-            //            ClientCertificates = new[] { certificate }
-            //        };
-            //        logs += certificate.Subject;
-            //        logs += "--- Certificate has been added. \n";
-            //    }
-            //}
-            //switch (connectionParameters.AuthenticationMechanism)
-            //{
-            //    case Models.MongoAuthentication.SCRAM:
-            //        break;
-            //    case Models.MongoAuthentication.X509:
-            //        settings.VerifySslCertificate = !connectionParameters.IsSelfSignedEnabled;
-            //        settings.UseSsl = true;
-            //        connectionParameters.SshPrivateKeyData = FilePathHelper.ReadFile(Server, connectionParameters.SshPrivateKey);
-            //        var certificate = string.IsNullOrEmpty(connectionParameters.SshUserName) ? new X509Certificate2(connectionParameters.SshPrivateKeyData) : new X509Certificate2(connectionParameters.SshPrivateKeyData, connectionParameters.SshUserName);
-            //        settings.SslSettings = new SslSettings()
-            //        {
-            //            ClientCertificates = new[] { certificate }
-            //        };
-            //        settings.Credential = MongoCredential.CreateMongoX509Credential(certificate.Subject.ToString().Replace("S=", "ST=").Replace(", ", ","));
-            //        logs += "--- switch called for x509 \n";
-            //        break;
-            //    default:
-            //        break;
-            //}
-            //logs += "--- settings has been returned. \n";
+            };        
             return settings;
         }
     }
